a23e507a68fc60c2f79bdcf4027370e78ef5124d
sherinasundarrajan123 <37038644+sherinasundarrajan123@users.noreply.github.com>
2018-12-03 21:39:48 +0530

    Create EmailVAlidation.java

---
+++ EmailVAlidation.java
@@ -0,0 +1,68 @@
+package emailvalidation1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sherina Sundarrajan
+ */
+public class EmailValidation1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        int i=0,flag1=0,flag2=0;
+        String email=sc.nextLine();
+        String gmailValidation="@gmail.com";
+        char[] mail=email.toCharArray();
+        while(i<mail.length&&mail[i]!='@')
+        {
+            if(i==0&&(mail[i]<97||mail[i]>122))
+            {
+                System.out.println("Invalid mail ID");
+                System.exit(0);
+            }
+            else if(i!=0&&mail[i]=='_'&&flag1==0)
+            {
+                flag2=1;
+            } 
+            else if(i!=0&&(mail[i]>=48&&mail[i]<=56)&&flag2==0)
+            {
+                flag1=1;
+            }
+            else if(i!=0&&(((mail[i]>=48&&mail[i]<=56)&&flag2!=0))||(mail[i]=='_'&&flag1!=0))
+            {
+                System.out.println("Invalid mail ID");
+                System.exit(0);
+            }
+            i++;
+        }
+        if(i>=mail.length)
+        {
+            System.out.println("Invalid mail ID");
+            System.exit(0);
+        }
+        char[] checkerGmail=gmailValidation.toCharArray();
+        int j=0;
+        while(i<mail.length)
+        {
+            if(checkerGmail[j]!=mail[i])
+            {
+                System.out.println("Invalid mail ID");
+                System.exit(0);
+            }
+            i++;
+            j++;
+        }
+        if(j<checkerGmail.length)
+        {
+            System.out.println("Invalid mail ID");
+            System.exit(0);
+        }
+        System.out.println("Valid mail");
+    }
+    
+}
